cb44fb895d38f80066a4fe5f1f602e5f4fce7cbb
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-02-15 21:49:10 -0300

    feat: add next-compose-plugins and next-transpile-modules packages

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,12 +1,13 @@
 import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/design-system/components/Text";
 
 export default function HomeScreen() {
   return (
     <main>
-      <h1>Home</h1>
-      <p>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">
         Importando modulo local: @alura/utils/math/sum sum(2, 2) = {sum(2, 2)}
-      </p>
+      </Text>
     </main>
   );
 }
