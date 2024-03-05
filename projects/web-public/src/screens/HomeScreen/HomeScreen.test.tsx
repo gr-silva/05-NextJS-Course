aa57a8f6d68e8e1059ffb65a39aec79f13ac5915
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-03-04 21:42:17 -0300

    feat: add HomeScreen test

---
+++ projects/web-public/src/screens/HomeScreen/HomeScreen.test.tsx
@@ -0,0 +1,11 @@
+import { customRender } from "@alura/test-commons/react-testing-library";
+import { HomeScreen } from "./HomeScreen";
+
+const render = customRender();
+
+describe("<HomeScreen />", () => {
+  it("renders the page", () => {
+    const { container } = render(<HomeScreen />);
+    expect(container).toMatchSnapshot();
+  });
+});
--- setup/test-commons/base.ts
+++ setup/test-commons/base.ts
@@ -1,14 +1,18 @@
 import type { Config } from "@jest/types";
-import path from 'path';
+import path from "path";
 
 // Sync object
 const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   verbose: true,
-  transform:{
-    '\\.[jt]sx?$': ['babel-jest', {
-      configFile: path.resolve(__dirname, '.babelrc')
-    }]
-  }
-}
-export default config
+  setupFilesAfterEnv: [path.resolve(__dirname, "src", "setupTests.js")],
+  transform: {
+    "\\.[jt]sx?$": [
+      "babel-jest",
+      {
+        configFile: path.resolve(__dirname, ".babelrc"),
+      },
+    ],
+  },
+};
+export default config;
